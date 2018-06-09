a1a83409ce7eb056579a1298e9ba9e0e37b0008c
Gabriel Rumbaut <grumbaut@gmail.com>
2018-06-09 17:51:54 -0400

    commit

--- components/UserRequests.js
+++ components/UserRequests.js
@@ -52,7 +52,7 @@ class UserRequests extends Component {
                       raised
                       buttonStyle={{ backgroundColor: 'green', borderRadius: 10, marginTop: 15 }}
                       title='Chat'
-                      onPress={() => this.props.navigation.navigate('Chat', { sender: requesterId, receiver: responderId })}
+                      onPress={() => this.props.navigation.navigate('Chat', { sender: requesterId, receiver: requesterId })}
                     />
                   )
                 }
